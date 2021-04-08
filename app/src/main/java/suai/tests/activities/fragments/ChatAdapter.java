41df4d0d32b3043076d237bc68dbd56c4fd1385e
LizzVs <eliz.vasilieva@yandex.ru>
2021-04-08 16:25:05 +0300

    Создан адаптер для RecyclerView чатов

---
+++ app/src/main/java/suai/tests/activities/fragments/ChatAdapter.java
@@ -0,0 +1,58 @@
+package suai.tests.activities.fragments;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import suai.tests.R;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
+
+    private final LayoutInflater inflater;
+    private final List<ChatClass> chats;
+
+    ChatAdapter(Context context, List<ChatClass> chats) {
+        this.chats = chats;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public ChatAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.chat_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ChatAdapter.ViewHolder holder, int position) {
+        ChatClass chat = chats.get(position);
+        holder.imageView.setImageResource(chat.getPhoto());
+        holder.nameView.setText(chat.getName());
+        holder.dateView.setText(chat.getLastMessage().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return chats.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        final ImageView imageView;
+        final TextView nameView, dateView;
+
+        ViewHolder(View view){
+            super(view);
+            imageView = (ImageView)view.findViewById(R.id.image);
+            nameView = (TextView)view.findViewById(R.id.name);
+            dateView = (TextView)view.findViewById(R.id.date);
+        }
+
+    }
+}
---
+++ app/src/main/java/suai/tests/activities/fragments/ChatClass.java
@@ -0,0 +1,42 @@
+package suai.tests.activities.fragments;
+
+import android.media.Image;
+
+import java.util.Date;
+
+public class ChatClass {
+    private String name;
+    private Integer photo;
+    private Date lastMessage;
+
+    public  ChatClass(String name, Integer photo, Date lastMessage)
+    {
+        this.name = name;
+        this.photo = photo;
+        this.lastMessage = lastMessage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Integer photo) {
+        this.photo = photo;
+    }
+
+    public Date getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(Date lastMessage) {
+        this.lastMessage = lastMessage;
+    }
+}
