290028fb34409c4eca3a58a4078bd3004ed9902a
LizzVs <eliz.vasilieva@yandex.ru>
2021-04-27 11:52:08 +0300

    Add function for sending message

--- app/src/main/java/suai/tests/activities/fragments/ChatFragment.java
+++ app/src/main/java/suai/tests/activities/fragments/ChatFragment.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -59,11 +61,26 @@ public class ChatFragment extends Fragment
             }
         });
 
+        EditText message = root.findViewById(R.id.editTextMessage);
         ImageButton buttonSendMessage = root.findViewById(R.id.imageButtonSend);
         buttonSendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (message.getText().length()!=0)
+                {
+                    Call<String[]> call = service.createMessage(idChat.toString(),String.valueOf(AccountFragment.idUser), message.getText().toString());
+                    call.enqueue(new Callback<String[]>() {
+                        @Override
+                        public void onResponse(Call<String[]> call, Response<String[]> response) {
+                            Log.v("result",response.body()[0]);
+                        }
 
+                        @Override
+                        public void onFailure(Call<String[]> call, Throwable t) {
+                            Log.v("result",t.getMessage());
+                        }
+                    });
+                }
             }
         });
 
