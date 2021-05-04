cb4bed64097f808f2970b0c5d2f8acb43ce09393
LizzVs <eliz.vasilieva@yandex.ru>
2021-05-04 18:30:44 +0300

    Add viewing read and unread messages in chat

--- app/src/main/java/suai/tests/common/adapters/MessagesAdapter.java
+++ app/src/main/java/suai/tests/common/adapters/MessagesAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -55,6 +56,12 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHo
             holder.messageView.setLayoutParams(param);*/
             holder.messageView.setBackgroundResource(R.drawable.message_box_post);
         }
+        if (Integer.parseInt(message.getMessages()[3])==1 && Integer.parseInt(message.getMessages()[0])==AccountFragment.idUser)
+            holder.readView.setImageResource(R.drawable.ic_message_read);
+        else if (Integer.parseInt(message.getMessages()[3])==0 && Integer.parseInt(message.getMessages()[0])==AccountFragment.idUser)
+            holder.readView.setImageResource(R.drawable.ic_message_post);
+        else if (Integer.parseInt(message.getMessages()[0])!=AccountFragment.idUser)
+            holder.readView.setVisibility(View.INVISIBLE);
         holder.dateView.setText(message.getMessages()[1]);
         holder.itemView.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -73,12 +80,14 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHo
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // final ImageView imageView;
         final TextView messageView, dateView;
+        final ImageView readView;
 
         ViewHolder(View view){
             super(view);
             //  imageView = (ImageView)view.findViewById(R.id.image);
             dateView = (TextView)view.findViewById(R.id.date);
             messageView = (TextView)view.findViewById(R.id.message);
+            readView = (ImageView)view.findViewById(R.id.read);
         }
 
     }
