b727cfb8ef293526a760d7278f8f08edee977e42
RPG Project <724danil.ivanov02@gmail.com>
2021-05-14 15:23:45 +0300

    Create students statistic

--- app/src/main/java/suai/tests/activities/fragments/StatisticsFragment.java
+++ app/src/main/java/suai/tests/activities/fragments/StatisticsFragment.java
@@ -1,22 +1,10 @@
 package suai.tests.activities.fragments;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.Rect;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.LayerDrawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.RoundRectShape;
-import android.graphics.drawable.shapes.Shape;
 import android.os.Bundle;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -27,32 +15,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Space;
 import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.ResourceBundle;
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import suai.tests.R;
-import suai.tests.activities.MainActivity;
-import suai.tests.common.AlertDialogBuilder;
+import suai.tests.common.AndroidElementsBuilder;
 import suai.tests.common.api.RetrofitConnection;
 import suai.tests.common.api.commonAPI;
 import suai.tests.common.api.pojo.common.ItemsPOJO;
-import suai.tests.common.api.testsAPI;
 
 public class StatisticsFragment extends Fragment
 {
@@ -61,6 +49,9 @@ public class StatisticsFragment extends Fragment
     private Spinner StatisticsSubjectsSpinner;
 
     private TableLayout MarksTableLayout;
+    private PieChart StatisticsPieChart;
+
+    private TextView StatisticsStudentAverageMark;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -87,8 +78,11 @@ public class StatisticsFragment extends Fragment
 
         BottomNavigationView navBar = getActivity().findViewById(R.id.nav_view); navBar.setVisibility(View.GONE);
 
-        StatisticsSubjectsSpinner = root.findViewById(R.id.StatisticsSubjectsSpinner);
-        MarksTableLayout = root.findViewById(R.id.MarksTableLayout);
+        StatisticsSubjectsSpinner = root.findViewById(R.id.StatisticsSpinner);
+        MarksTableLayout = root.findViewById(R.id.StudentMarksTableLayout);
+        StatisticsPieChart = root.findViewById(R.id.StatisticsStudentMarksPieChart);
+
+        StatisticsStudentAverageMark = root.findViewById(R.id.StatisticsStudentAverageMark);
 
         commonAPI service = RetrofitConnection.commonAPI; Call<ItemsPOJO[]> call;
 
@@ -155,118 +149,15 @@ public class StatisticsFragment extends Fragment
         StatisticsSubjectsSpinner.setOnItemSelectedListener(itemSelectedListener);
     }
 
-    private TextView createTextView(LinearLayout container, String text)
-    {
-        TextView dataTextView = new TextView(container.getContext()); dataTextView.setPadding(4, 4, 4, 4);
-        dataTextView.setGravity(Gravity.CENTER); dataTextView.setTextColor(getResources().getColor(R.color.black));
-        dataTextView.setTextSize(18); dataTextView.setText(text); dataTextView.setSingleLine(false);
-
-        dataTextView.setLayoutParams(
-                new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 128, 1f));
-
-        return dataTextView;
-    }
-
-    private ImageView createImageView(LinearLayout container, int resourceID, int tintID)
-    {
-        ImageView dataImageView = new ImageView(container.getContext());
-        dataImageView.setImageResource(resourceID);
-        dataImageView.setColorFilter(ContextCompat.getColor(container.getContext(), tintID), PorterDuff.Mode.SRC_IN);
-        dataImageView.setPadding(4, 4, 4, 4);
-        dataImageView.setLayoutParams(
-                new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 128, 1f));
-
-        return dataImageView;
-    }
-
-    private LayerDrawable setCellBorders(boolean left, boolean top, boolean right, boolean bottom)
-    {
-        LayerDrawable borders = (LayerDrawable) getResources().getDrawable(R.drawable.item_table_borders);
-        Drawable borderTop =  borders.findDrawableByLayerId(R.id.itemTableBorderTop),
-                 borderBottom =  borders.findDrawableByLayerId(R.id.itemTableBorderBottom),
-                 borderLeft =  borders.findDrawableByLayerId(R.id.itemTableBorderLeft),
-                 borderRight =  borders.findDrawableByLayerId(R.id.itemTableBorderRight);
-
-        if (!top) { borderTop.setAlpha(0); }
-        if (!bottom) { borderBottom.setAlpha(0); }
-        if (!left) { borderLeft.setAlpha(0); }
-        if (!right) { borderRight.setAlpha(0); }
-
-        return borders;
-    }
-
-    public void setTableBorders(View view, int rowsNumber, int cellsNumber, int rowIndex, int cellIndex)
-    {
-        if (rowIndex == 0)
-        {
-            if (rowsNumber == 1)
-            {
-                if (cellsNumber == 1) { view.setBackground(setCellBorders(false, false, false, false)); }
-                else
-                {
-                    if (cellIndex == 0) { view.setBackground(setCellBorders(false, false, false, false)); }
-                    else { view.setBackground(setCellBorders(true, false, false, false)); }
-                }
-            }
-            else
-            {
-                if (cellsNumber == 1) { view.setBackground(setCellBorders(false, false, false, false)); }
-                else
-                {
-                    if (cellIndex == 0) { view.setBackground(setCellBorders(false, false, false, true)); }
-                    else { view.setBackground(setCellBorders(true, false, false, true)); }
-                }
-            }
-        }
-        else
-        {
-            if (rowIndex + 1 == rowsNumber)
-            {
-                if (cellsNumber == 1) { view.setBackground(setCellBorders(false, false, false, false)); }
-                else
-                {
-                    if (cellIndex == 0) { view.setBackground(setCellBorders(false, false, false, false)); }
-                    else { view.setBackground(setCellBorders(true, false, false, false)); }
-                }
-            }
-            else
-            {
-                if (cellIndex == 0) { view.setBackground(setCellBorders(false, false, false, true)); }
-                else { view.setBackground(setCellBorders(true, false, false, true)); }
-            }
-        }
-    }
-
-    private TableRow createTableHeader(Context context, String[] headerData)
-    {
-        TableRow tableRow; LinearLayout linearLayout; TextView cell;
-
-        tableRow = new TableRow(context); tableRow.setGravity(Gravity.CENTER);
-        linearLayout = new LinearLayout(tableRow.getContext()); linearLayout.setOrientation(LinearLayout.HORIZONTAL);
-        linearLayout.setGravity(Gravity.CENTER);
-
-        cell = createTextView(linearLayout, "");
-        LayerDrawable borders = (LayerDrawable) getResources().getDrawable(R.drawable.item_table_header_borders);
-        cell.setBackground(borders); linearLayout.addView(cell);
-
-        for (int i = 0; i < headerData.length; i++)
-        {
-            cell = createTextView(linearLayout, headerData[i]);
-            cell.setBackground(setCellBorders(true, false, false, true));
-            linearLayout.addView(cell);
-        }
-
-        tableRow.addView(linearLayout);
-        return tableRow;
-    }
-
     private void setStudentStatistics(View root, ItemsPOJO[] marks)
     {
         MarksTableLayout.setStretchAllColumns(true); MarksTableLayout.setShrinkAllColumns(true);
         TableRow tableRow; LinearLayout linearLayout; ImageView cellImage = null; TextView cell; int status, cellsIndex;
 
         String[] titles = { "Оценка / статус" };
-        MarksTableLayout.addView(createTableHeader(MarksTableLayout.getContext(), titles));
+        MarksTableLayout.addView(AndroidElementsBuilder.createTableHeader(MarksTableLayout.getContext(), titles));
+
+        int inProgressCounter = 0, errorsCounter = 0, mark_2_counter = 0, mark_3_counter = 0, mark_4_counter = 0, mark_5_counter = 0;
 
         for (int i = 0; i < marks.length; i++)
         {
@@ -274,28 +165,91 @@ public class StatisticsFragment extends Fragment
             linearLayout = new LinearLayout(tableRow.getContext()); linearLayout.setOrientation(LinearLayout.HORIZONTAL);
 
             cellsIndex = 0; status = Integer.parseInt(marks[i].getItems()[1]);
-            cell = createTextView(linearLayout, marks[i].getItems()[0]); linearLayout.addView(cell);
-            setTableBorders(cell, marks.length, 2, i, cellsIndex); cellsIndex++;
+            cell = AndroidElementsBuilder.createTextView(linearLayout, marks[i].getItems()[0]); linearLayout.addView(cell);
+            AndroidElementsBuilder.setTableBorders(cell, marks.length, 2, i, cellsIndex); cellsIndex++;
 
             if (status == 0)
             {
-                cellImage = createImageView(linearLayout,
+                cellImage = AndroidElementsBuilder.createImageView(linearLayout.getContext(),
                         R.drawable.ic_baseline_access_time_24, android.R.color.holo_orange_light);
+                inProgressCounter++;
             }
 
             if (status == 2)
             {
-                cellImage = createImageView(linearLayout,
+                cellImage = AndroidElementsBuilder.createImageView(linearLayout.getContext(),
                         R.drawable.ic_baseline_error_24, android.R.color.holo_red_light);
+                errorsCounter++;
             }
 
-            if (status == 1) { cell = createTextView(linearLayout, marks[i].getItems()[2]); }
+            if (status == 1)
+            {
+                cell = AndroidElementsBuilder.createTextView(linearLayout, marks[i].getItems()[2]);
+                if (marks[i].getItems()[2].equals("2")) { mark_2_counter++; }
+                if (marks[i].getItems()[2].equals("3")) { mark_3_counter++; }
+                if (marks[i].getItems()[2].equals("4")) { mark_4_counter++; }
+                if (marks[i].getItems()[2].equals("5")) { mark_5_counter++; }
+            }
 
-            if (status == 1) { setTableBorders(cell, marks.length, 2, i, cellsIndex); linearLayout.addView(cell); }
-            else { setTableBorders(cellImage, marks.length, 2, i, cellsIndex); linearLayout.addView(cellImage); }
+            if (status == 1)
+            {
+                AndroidElementsBuilder.setTableBorders(cell, marks.length, 2, i, cellsIndex);
+                linearLayout.addView(cell);
+            }
+            else
+            {
+                AndroidElementsBuilder.setTableBorders(cellImage, marks.length, 2, i, cellsIndex);
+                linearLayout.addView(cellImage);
+            }
 
             tableRow.addView(linearLayout); MarksTableLayout.addView(tableRow);
         }
+
+        int averageMark = ((2 * mark_2_counter) + (3 * mark_3_counter) +
+                           (4 * mark_4_counter) + (5 * mark_5_counter)) /
+                           (mark_2_counter + mark_3_counter + mark_4_counter + mark_5_counter);
+        StatisticsStudentAverageMark.setText(String.valueOf(averageMark)); int colorID = android.R.color.secondary_text_dark;
+        if (averageMark < 2) { StatisticsStudentAverageMark.setText("Нет принятых лабораторных работ"); }
+        if (averageMark < 3) { colorID = android.R.color.holo_red_dark; }
+        if (averageMark < 4) { colorID = android.R.color.holo_red_light; }
+        if (averageMark < 5) { colorID = android.R.color.holo_green_dark; }
+        if (averageMark == 5) { colorID = android.R.color.holo_green_light; }
+        StatisticsStudentAverageMark.setTextColor(getResources().getColor(colorID));
+
+        ArrayList<PieChartItem> pieChartDataArrayList = new ArrayList<PieChartItem>();
+        if (inProgressCounter != 0) { pieChartDataArrayList.add(new PieChartItem("В прогрессе", inProgressCounter)); }
+        if (errorsCounter != 0) { pieChartDataArrayList.add(new PieChartItem("Ошибки", errorsCounter)); }
+        if (mark_2_counter != 0) { pieChartDataArrayList.add(new PieChartItem("Неудовлетворительно (2)", mark_2_counter)); }
+        if (mark_3_counter != 0) { pieChartDataArrayList.add(new PieChartItem("Удовлетворительно (3)", mark_3_counter)); }
+        if (mark_4_counter != 0) { pieChartDataArrayList.add(new PieChartItem("Хорошо (4)", mark_4_counter)); }
+        if (mark_5_counter != 0) { pieChartDataArrayList.add(new PieChartItem("Отлично (5)", mark_5_counter)); }
+
+        ArrayList<PieEntry> pieChartEntries = new ArrayList<PieEntry>();
+        for (int i = 0; i < pieChartDataArrayList.size(); i++)
+        {
+            pieChartEntries.add(new PieEntry(pieChartDataArrayList.get(i).getMarksCount(),
+                                             pieChartDataArrayList.get(i).getTitle()));
+        }
+
+        PieDataSet pieChartDataSet = new PieDataSet(pieChartEntries, "Количество оценок или статусов по данной дисциплине");
+        pieChartDataSet.setColors(ColorTemplate.COLORFUL_COLORS); pieChartDataSet.setValueTextSize(16);
+        pieChartDataSet.setValueTextColor(R.color.suai_primary);
+        pieChartDataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        pieChartDataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+
+        PieData data = new PieData(pieChartDataSet);
+        Description chartDescription = new Description(); chartDescription.setText("");
+        StatisticsPieChart.setDescription(chartDescription); StatisticsPieChart.setData(data);
+
+        Legend legend = StatisticsPieChart.getLegend();
+        legend.setTextSize(12); legend.setDrawInside(false); legend.setWordWrapEnabled(true);
+        legend.setTextColor(getResources().getColor(R.color.suai_secondary));
+        legend.setXEntrySpace(128);
+
+
+        StatisticsPieChart.setCenterText("Оценки по предмету \"" + StatisticsSubjectsSpinner.getSelectedItem().toString() + "\"");
+        StatisticsPieChart.setEntryLabelColor(R.color.suai_primary);
+        StatisticsPieChart.animateXY(1000,1000); StatisticsPieChart.invalidate();
     }
 
     private void setTeacherStatistics(View root, ItemsPOJO[] resp)
@@ -303,18 +257,15 @@ public class StatisticsFragment extends Fragment
 
     }
 
-    class CustomShapeDrawable extends ShapeDrawable {
-        private final Paint fillpaint;
+    private static class PieChartItem {
+        String title; int marksCount;
 
-        public CustomShapeDrawable(Shape s, int fill) {
-            super(s);
-            fillpaint = new Paint(this.getPaint());
-            fillpaint.setColor(fill);
-        }
+        public PieChartItem(String title, int marksCount) { this.title = title; this.marksCount = marksCount; }
 
-        @Override
-        protected void onDraw(Shape shape, Canvas canvas, Paint paint) {
-            shape.draw(canvas, fillpaint);
-        }
+        public String getTitle() { return title; }
+        public void setTitle(String title) { this.title = title; }
+
+        public int getMarksCount() { return marksCount; }
+        public void setMarksCount(int marksCount) { this.marksCount = marksCount; }
     }
 }
---
+++ app/src/main/java/suai/tests/common/AndroidElementsBuilder.java
@@ -0,0 +1,134 @@
+package suai.tests.common;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import suai.tests.R;
+
+public class AndroidElementsBuilder extends AppCompatActivity
+{
+    private static Context context;
+    public static Context getContext() { return context; }
+    public static void setContext(Context _context) { context = _context; }
+
+    private static Resources getElementsBuilderResources() { return getContext().getResources(); }
+
+    public static TextView createTextView(LinearLayout container, String text)
+    {
+        TextView dataTextView = new TextView(container.getContext()); dataTextView.setPadding(4, 4, 4, 4);
+        dataTextView.setGravity(Gravity.CENTER); dataTextView.setTextColor(getElementsBuilderResources().getColor(R.color.black));
+        dataTextView.setTextSize(18); dataTextView.setText(text); dataTextView.setSingleLine(false);
+
+        dataTextView.setLayoutParams(
+                new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 128, 1f));
+
+        return dataTextView;
+    }
+
+    public static ImageView createImageView(Context context, int resourceID, int tintID)
+    {
+        ImageView dataImageView = new ImageView(context);
+        dataImageView.setImageResource(resourceID);
+        dataImageView.setColorFilter(ContextCompat.getColor(context, tintID), PorterDuff.Mode.SRC_IN);
+        dataImageView.setPadding(4, 4, 4, 4);
+        dataImageView.setLayoutParams(
+                new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 128, 1f));
+
+        return dataImageView;
+    }
+
+    public static LayerDrawable setCellBorders(boolean left, boolean top, boolean right, boolean bottom)
+    {
+        LayerDrawable borders = (LayerDrawable) getElementsBuilderResources().getDrawable(R.drawable.item_table_borders);
+        Drawable borderTop =  borders.findDrawableByLayerId(R.id.itemTableBorderTop),
+                borderBottom =  borders.findDrawableByLayerId(R.id.itemTableBorderBottom),
+                borderLeft =  borders.findDrawableByLayerId(R.id.itemTableBorderLeft),
+                borderRight =  borders.findDrawableByLayerId(R.id.itemTableBorderRight);
+
+        if (!top) { borderTop.setAlpha(0); }
+        if (!bottom) { borderBottom.setAlpha(0); }
+        if (!left) { borderLeft.setAlpha(0); }
+        if (!right) { borderRight.setAlpha(0); }
+
+        return borders;
+    }
+
+    public static void setTableBorders(View view, int rowsNumber, int cellsNumber, int rowIndex, int cellIndex)
+    {
+        if (rowIndex == 0)
+        {
+            if (rowsNumber == 1)
+            {
+                if (cellsNumber == 1) { view.setBackground(setCellBorders(false, false, false, false)); }
+                else
+                {
+                    if (cellIndex == 0) { view.setBackground(setCellBorders(false, false, false, false)); }
+                    else { view.setBackground(setCellBorders(true, false, false, false)); }
+                }
+            }
+            else
+            {
+                if (cellsNumber == 1) { view.setBackground(setCellBorders(false, false, false, false)); }
+                else
+                {
+                    if (cellIndex == 0) { view.setBackground(setCellBorders(false, false, false, true)); }
+                    else { view.setBackground(setCellBorders(true, false, false, true)); }
+                }
+            }
+        }
+        else
+        {
+            if (rowIndex + 1 == rowsNumber)
+            {
+                if (cellsNumber == 1) { view.setBackground(setCellBorders(false, false, false, false)); }
+                else
+                {
+                    if (cellIndex == 0) { view.setBackground(setCellBorders(false, false, false, false)); }
+                    else { view.setBackground(setCellBorders(true, false, false, false)); }
+                }
+            }
+            else
+            {
+                if (cellIndex == 0) { view.setBackground(setCellBorders(false, false, false, true)); }
+                else { view.setBackground(setCellBorders(true, false, false, true)); }
+            }
+        }
+    }
+
+    public static TableRow createTableHeader(Context context, String[] headerData)
+    {
+        TableRow tableRow; LinearLayout linearLayout; TextView cell;
+
+        tableRow = new TableRow(context); tableRow.setGravity(Gravity.CENTER);
+        linearLayout = new LinearLayout(tableRow.getContext()); linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        linearLayout.setGravity(Gravity.CENTER);
+
+        cell = createTextView(linearLayout, "");
+        LayerDrawable borders = (LayerDrawable) getElementsBuilderResources().getDrawable(R.drawable.item_table_header_borders);
+        cell.setBackground(borders); linearLayout.addView(cell);
+
+        for (int i = 0; i < headerData.length; i++)
+        {
+            cell = createTextView(linearLayout, headerData[i]);
+            cell.setBackground(setCellBorders(true, false, false, true));
+            linearLayout.addView(cell);
+        }
+
+        tableRow.addView(linearLayout);
+        return tableRow;
+    }
+}
